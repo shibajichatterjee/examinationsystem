dcd88dfde2e12d6b1f30632017521239176cbf40
xyz <xyz@xyz-PC>
2019-03-31 23:10:54 +0530

    commit

--- src/com/rest/examinationsystem/assesment/item/controller/AssesmentItemController.java
+++ src/com/rest/examinationsystem/assesment/item/controller/AssesmentItemController.java
@@ -1,7 +1,11 @@
 package com.rest.examinationsystem.assesment.item.controller;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.ParseException;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,7 +18,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.rest.examinationsystem.assesment.item.dto.AssesmentItemDto;
 import com.rest.examinationsystem.assesment.item.service.AssesmentItemService;
+import com.rest.examinationsystem.assessment.item.options.dto.AssesmentItemOptionDto;
+import com.rest.examinationsystem.assessment.item.options.service.AssesmentItemOptionService;
 import com.rest.examinationsystem.util.DateUtil;
 import com.rest.examinationsystem.util.ExcelUtil;
 import com.rest.examinationsystem.util.ImageUtil;
@@ -23,109 +30,77 @@ import com.rest.framework.constant.MessageEnum;
 import com.rest.framework.exception.NoRecordsFoundException;
 import com.rest.framework.exception.UnauthorizedException;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(value = "/assesmentItem")
+@Api(value = "Excel Upload", description = "Operations For Excel Upload")
+
 @Transactional
 public class AssesmentItemController {
 
 	@Autowired
 	private AssesmentItemService assesmentItemService;
-	
+	@Autowired
+	private AssesmentItemOptionService assesmentItemOptionService;
+
 	@Value("${assesmentFilePath}")
 	private String assesmentFilePath;
-	
+
 	@Autowired
 	private ExcelUtil excelUtil;
 
-//	@RequestMapping(value = "/createAssesmentItem", method = RequestMethod.POST)
-//	public ResponseEntity createAssesmentItem(@RequestHeader("API-KEY") String apiKey)
-//			throws UnauthorizedException, ParseException, IOException {
-//		if (!apiKey.equals(MessageEnum.API_KEY)) {
-//			throw new UnauthorizedException(MessageEnum.unathorized);
-//		}
-//		ResponseBean responseBean = new ResponseBean();
-//		responseBean.setBody(MessageEnum.enumMessage.SUCESS.getMessage());
-//		return new ResponseEntity(responseBean, org.springframework.http.HttpStatus.OK);
-//	}
-	
-	
+	@ApiOperation(value = "Create Assesment Item with option")
 	@RequestMapping(value = "/createAssesmentItem", method = RequestMethod.POST)
-	public ResponseEntity createAssesmentItem()
-			throws UnauthorizedException, ParseException, IOException {
-		
-		System.out.println(assesmentFilePath);
-		
-		
-		excelUtil.convertExcelToItemObject(assesmentFilePath);
-		
-		
+	public ResponseEntity createAssesmentItem(@RequestHeader("API-KEY") String apiKey,
+			@RequestParam("excel") MultipartFile file) throws UnauthorizedException, ParseException, IOException {
+
 		ResponseBean responseBean = new ResponseBean();
+		if (!apiKey.equals(MessageEnum.API_KEY)) {
+			throw new UnauthorizedException(MessageEnum.unathorized);
+		}
+		//byte[] byteArray = ImageUtil.getByteArrayFromMaltipartFormData(file);
+		//InputStream targetStream = new ByteArrayInputStream(byteArray);
+		//Map<AssesmentItemDto, List<AssesmentItemOptionDto>> assesmentItemDtoMap= excelUtil.convertExcelToItemObject(targetStream);
+		
+		Map<AssesmentItemDto, List<AssesmentItemOptionDto>> assesmentItemDtoMap= excelUtil.convertExcelToItemObject(assesmentFilePath);
+
+		for(AssesmentItemDto assesmentItemDto:assesmentItemDtoMap.keySet())
+		{
+			assesmentItemService.createOrUpdate(assesmentItemDto);
+			for(AssesmentItemOptionDto assesmentItemOptionDto:assesmentItemDtoMap.get(assesmentItemDto))
+			{
+				assesmentItemOptionService.create(assesmentItemOptionDto);
+			}
+
+		}
+
 		responseBean.setBody(MessageEnum.enumMessage.SUCESS.getMessage());
 		return new ResponseEntity(responseBean, org.springframework.http.HttpStatus.OK);
 	}
-	
 
-	@RequestMapping(value = "/getAssesmentItemById", method = RequestMethod.GET)
-	public ResponseEntity getAssesmentItemById(@RequestHeader("API-KEY") String apiKey, @RequestParam("Id") long id)
-			throws UnauthorizedException, ParseException, IOException, NoRecordsFoundException {
-		if (!apiKey.equals(MessageEnum.API_KEY)) {
-			throw new UnauthorizedException(MessageEnum.unathorized);
-		}
-		ResponseBean responseBean = new ResponseBean();
-		return new ResponseEntity(responseBean, org.springframework.http.HttpStatus.OK);
-	}
+	/*
+	 * @RequestMapping(value = "/getAssesmentItemById", method =
+	 * RequestMethod.GET) public ResponseEntity
+	 * getAssesmentItemById(@RequestHeader("API-KEY") String
+	 * apiKey, @RequestParam("Id") long id) throws UnauthorizedException,
+	 * ParseException, IOException, NoRecordsFoundException { if
+	 * (!apiKey.equals(MessageEnum.API_KEY)) { throw new
+	 * UnauthorizedException(MessageEnum.unathorized); } ResponseBean
+	 * responseBean = new ResponseBean(); return new
+	 * ResponseEntity(responseBean, org.springframework.http.HttpStatus.OK); }
+	 * 
+	 * @RequestMapping(value = "/updateAssesmentItem", method =
+	 * RequestMethod.POST) public ResponseEntity
+	 * updateUser(@RequestHeader("API-KEY") String apiKey) throws
+	 * UnauthorizedException, ParseException, IOException,
+	 * NoRecordsFoundException {
+	 * 
+	 * if (!apiKey.equals(MessageEnum.API_KEY)) { throw new
+	 * UnauthorizedException(MessageEnum.unathorized); } ResponseBean
+	 * responseBean = new ResponseBean(); return new
+	 * ResponseEntity(responseBean, org.springframework.http.HttpStatus.OK); }
+	 */
 
-	@RequestMapping(value = "/updateAssesmentItem", method = RequestMethod.POST)
-	public ResponseEntity updateUser(@RequestHeader("API-KEY") String apiKey)
-			throws UnauthorizedException, ParseException, IOException, NoRecordsFoundException {
-
-		if (!apiKey.equals(MessageEnum.API_KEY)) {
-			throw new UnauthorizedException(MessageEnum.unathorized);
-		}
-		ResponseBean responseBean = new ResponseBean();
-		return new ResponseEntity(responseBean, org.springframework.http.HttpStatus.OK);
-	}
-
-	
-	//===================== Private Method =======================
-//	private UserDto mapUserRequestDtoToUserDto(UserDto userDto, MultipartFile file, String password,
-//			String emailAddress, String firstName, String middleName, String lastName, String adminId, String createdOn)
-//			throws ParseException, IOException {
-//		userDto.setAdminId(adminId);
-//		if(createdOn!=null && createdOn.length()>0) {
-//		 userDto.setCreatedOn(DateUtil.getFormattedDate(createdOn, "yyyy-MM-dd"));
-//		}
-//		userDto.setEmailAddress(emailAddress);
-//		userDto.setFirstName(firstName);
-//		userDto.setLastName(lastName);
-//		userDto.setMiddleName(middleName);
-//		userDto.setPassword(password);
-//		if (file != null) {
-//			userDto.setPhoto(ImageUtil.getByteArrayFromMaltipartFormData(file));
-//		}
-//		return userDto;
-//	}
-//
-//	private UserRequestDto mapUserRequestDtoToUserDto(UserDto userDto) throws ParseException {
-//		UserRequestDto userRequestDto = null;
-//		if (userDto != null) {
-//			userRequestDto = new UserRequestDto();
-//			userRequestDto.setAdminId(userDto.getAdminId());
-//			if (userDto.getCreatedOn() != null) {
-//				userRequestDto
-//						.setCreatedOn(DateUtil.getFormattedDateStringFromDate(userDto.getCreatedOn(), "yyyy-mm-dd"));
-//			}
-//			userRequestDto.setEmailAddress(userDto.getEmailAddress());
-//			userRequestDto.setFirstName(userDto.getFirstName());
-//			userRequestDto.setId(userDto.getId());
-//			userRequestDto.setLastName(userDto.getLastName());
-//			userRequestDto.setMiddleName(userDto.getMiddleName());
-//			userRequestDto.setPassword(userDto.getPassword());
-//			if (userDto.getPhoto() != null && userDto.getPhoto().length > 0) {
-//				userRequestDto.setPhoto(ImageUtil.getBase64FromByteArray(userDto.getPhoto()));
-//			}
-//		}
-//		return userRequestDto;
-//	}
 }
--- src/com/rest/examinationsystem/util/ExcelUtil.java
+++ src/com/rest/examinationsystem/util/ExcelUtil.java
@@ -3,6 +3,7 @@ package com.rest.examinationsystem.util;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -62,9 +63,38 @@ public class ExcelUtil {
 		}
 		return pictureMap;
 	}
+	public Map<Integer, Map<Integer, byte[]>> getImageIndex(InputStream inputStream) throws IOException {
+		Map<Integer, Map<Integer, byte[]>> pictureMap = new HashMap<>();
+		//FileInputStream fileInputStream = new FileInputStream(path);
+		XSSFWorkbook xssfWorkbook = new XSSFWorkbook(inputStream);
+		XSSFSheet sheet = xssfWorkbook.getSheetAt(0);
+		XSSFDrawing xssDrawing = sheet.getDrawingPatriarch();
+
+		if (null != xssDrawing) {
+			List<XSSFShape> shapes = xssDrawing.getShapes();
+			Map<Integer, byte[]> columnMap = Collections.emptyMap();
+			for (XSSFShape shape : shapes) {
+				if (shape instanceof XSSFPicture) {
+					XSSFPicture hssfPicture = (XSSFPicture) shape;
+					PictureData data = hssfPicture.getPictureData();
+					byte[] picData = data.getData();
+					int rowIndex = hssfPicture.getClientAnchor().getRow1();
+					int colIndex = hssfPicture.getClientAnchor().getCol1();
+					if (pictureMap.containsKey(rowIndex)) {
+						columnMap = pictureMap.get(rowIndex);
+					} else {
+						columnMap = new HashMap<>();
+					}
+					columnMap.put(colIndex, picData);
+					pictureMap.put(rowIndex, columnMap);
+				}
+			}
+		}
+		return pictureMap;
+	}
 	
 	
-	public void convertExcelToItemObject(String filePath) throws IOException {
+	public Map<AssesmentItemDto, List<AssesmentItemOptionDto>>   convertExcelToItemObject(String filePath) throws IOException {
 
 		Map<Integer, Map<Integer, byte[]>> imageMap =  getImageIndex(filePath);
 		List<String> headerList = Collections.emptyList();
@@ -236,6 +266,7 @@ public class ExcelUtil {
 		}
 		ObjectMapper mapper = new ObjectMapper();
 		System.out.println(mapper.writeValueAsString(assesmentItemDtoMap));
+		return assesmentItemDtoMap;
 	}
 	
 	private List<String> getHeader(Row row) {
@@ -253,6 +284,181 @@ public class ExcelUtil {
 			}
 		}
 		return headerList;
+	}
+	
+	public Map<AssesmentItemDto, List<AssesmentItemOptionDto>> convertExcelToItemObject(InputStream inputStream) throws IOException {
+
+		Map<Integer, Map<Integer, byte[]>> imageMap =  getImageIndex(inputStream);
+		List<String> headerList = Collections.emptyList();
+		//FileInputStream file = new FileInputStream(new File(filePath));
+
+		org.apache.poi.ss.usermodel.Workbook workbook = WorkbookFactory.create(inputStream);
+		org.apache.poi.ss.usermodel.Sheet sheet = workbook.getSheetAt(0);
+
+		Iterator<Row> rowIterator = sheet.rowIterator();
+		List<AssesmentItemOptionDto> assesmentItemOptionDtoList = null;
+		Map<AssesmentItemDto, List<AssesmentItemOptionDto>> assesmentItemDtoMap = new HashMap<>();
+		
+		AssesmentItemDto assesmentItemDto = null;
+		AssesmentItemOptionDto assesmentItemOptionDto  = null;
+				
+		int rowIndex =0;
+		while (rowIterator.hasNext()) {
+			Row row = rowIterator.next();
+			assesmentItemDto = new AssesmentItemDto();
+			
+			if (0 == rowIndex) {
+				headerList = getHeader(row);
+			} else {
+				int emptyColumn = 0;
+				assesmentItemOptionDtoList = new ArrayList<>();
+				for (int j = 0; j < headerList.size(); j++) {
+					Cell cell = row.getCell(j);	
+					 assesmentItemOptionDto = new AssesmentItemOptionDto();
+					if (cell == null || cell.getCellType() == CellType.BLANK) {
+                    	if(imageMap!=null && imageMap.containsKey(rowIndex)) {
+                    		Map<Integer, byte[]> imageColumnMap = imageMap.get(rowIndex);
+                    		if(imageColumnMap.containsKey(j)) {
+                    			 if(j==4) {
+                    				 assesmentItemDto.setAssessmentItemImage(imageColumnMap.get(j));
+                    			 }
+                    			 if(j==7) {
+                    				// assesmentItemOptionDto = new AssesmentItemOptionDto();
+                    				 assesmentItemOptionDto.setOptionImage(imageColumnMap.get(j));
+                    				 assesmentItemOptionDtoList.add(assesmentItemOptionDto);
+                    			 }
+                    			 if(j==8) {
+                    				 //assesmentItemOptionDto = new AssesmentItemOptionDto();
+                    				 assesmentItemOptionDto.setOptionImage(imageColumnMap.get(j));
+                    				 assesmentItemOptionDtoList.add(assesmentItemOptionDto);
+                    			 }
+                    			 if(j==9) {
+                    				 //assesmentItemOptionDto = new AssesmentItemOptionDto();
+                    				 assesmentItemOptionDto.setOptionImage(imageColumnMap.get(j));
+                    				 assesmentItemOptionDtoList.add(assesmentItemOptionDto);
+                    			 }
+                    			 if(j==10) {
+                    				 //assesmentItemOptionDto = new AssesmentItemOptionDto();
+                    				 assesmentItemOptionDto.setOptionImage(imageColumnMap.get(j));
+                    				 assesmentItemOptionDtoList.add(assesmentItemOptionDto);
+                    			 }
+                    			 if(j==11) {
+                    				 //assesmentItemOptionDto = new AssesmentItemOptionDto();
+                    				 assesmentItemOptionDto.setOptionImage(imageColumnMap.get(j));
+                    				 assesmentItemOptionDtoList.add(assesmentItemOptionDto);
+                    			 }
+                    		}else {
+                    			emptyColumn++;
+                    		}
+                    	}else {
+                    		emptyColumn++;	
+                    	}
+                    }else {                 	
+                    	if(cell.getCellType() == CellType.STRING) {
+                    		if(j==0) {
+                        		assesmentItemDto.setAssessmentItemTopic(cell.getStringCellValue());
+                        	}
+                        	if(j==1) {
+                        		assesmentItemDto.setAssessmentItemGivenId(cell.getStringCellValue());
+                        	}
+                        	if(j==2) {
+                        		assesmentItemDto.setAssesmentItemType(cell.getStringCellValue());
+                        	}
+                        	if(j==3) {
+                        		assesmentItemDto.setAssessmentItemText(cell.getStringCellValue());
+                        	}
+                        	if(j==4) {
+                        		assesmentItemDto.setAssessmentItemImage(null);
+                        	}
+                        	 if(j==7) {
+                        		 //assesmentItemOptionDto = new AssesmentItemOptionDto();
+                				 assesmentItemOptionDto.setOptionText(cell.getStringCellValue());
+                				 assesmentItemOptionDtoList.add(assesmentItemOptionDto);
+                			 }
+                			 if(j==8) {
+                				// assesmentItemOptionDto = new AssesmentItemOptionDto();
+                				 assesmentItemOptionDto.setOptionText(cell.getStringCellValue());
+                				 assesmentItemOptionDtoList.add(assesmentItemOptionDto);
+                			 }
+                			 if(j==9) {
+                				 //assesmentItemOptionDto = new AssesmentItemOptionDto();
+                				 assesmentItemOptionDto.setOptionText(cell.getStringCellValue());
+                				 assesmentItemOptionDtoList.add(assesmentItemOptionDto);
+                			 }
+                			 if(j==10) {
+                				// assesmentItemOptionDto = new AssesmentItemOptionDto();
+                				 assesmentItemOptionDto.setOptionText(cell.getStringCellValue());
+                				 assesmentItemOptionDtoList.add(assesmentItemOptionDto);
+                			 }
+                			 if(j==11) {
+                				 //assesmentItemOptionDto = new AssesmentItemOptionDto();
+                				 assesmentItemOptionDto.setOptionText(cell.getStringCellValue());
+                				 assesmentItemOptionDtoList.add(assesmentItemOptionDto);
+                			 }
+                			
+                    	}
+                    	if(cell.getCellType() == CellType.NUMERIC) {
+                    		
+                        	if(j==1) {
+                        		assesmentItemDto.setAssessmentItemGivenId(String.valueOf(cell.getNumericCellValue()));
+                        	}
+
+                        	if(j==3) {
+                        		assesmentItemDto.setAssessmentItemText(String.valueOf(cell.getNumericCellValue()));
+                        	}
+      
+                        	if(j==5) {
+                        		assesmentItemDto.setAssesmentItemNoOfOption((int)cell.getNumericCellValue());
+                        	}
+                        	if(j==6) {
+                        		assesmentItemDto.setCorrectOption((int)cell.getNumericCellValue());
+                        	}
+                        	 if(j==7) {
+                        		// assesmentItemOptionDto = new AssesmentItemOptionDto();
+                				 assesmentItemOptionDto.setOptionText(String.valueOf(cell.getNumericCellValue()));
+                				 assesmentItemOptionDtoList.add(assesmentItemOptionDto);
+                			 }
+                			 if(j==8) {
+                				// assesmentItemOptionDto = new AssesmentItemOptionDto();
+                				 assesmentItemOptionDto.setOptionText(String.valueOf(cell.getNumericCellValue()));
+                				 assesmentItemOptionDtoList.add(assesmentItemOptionDto);
+                			 }
+                			 if(j==9) {
+                				 //assesmentItemOptionDto = new AssesmentItemOptionDto();
+                				 assesmentItemOptionDto.setOptionText(String.valueOf(cell.getNumericCellValue()));
+                				 assesmentItemOptionDtoList.add(assesmentItemOptionDto);
+                			 }
+                			 if(j==10) {
+                				 assesmentItemOptionDto.setOptionText(String.valueOf(cell.getNumericCellValue()));
+                				 assesmentItemOptionDtoList.add(assesmentItemOptionDto);
+                			 }
+                			 if(j==11) {
+                				 assesmentItemOptionDto.setOptionText(String.valueOf(cell.getNumericCellValue()));
+                				 assesmentItemOptionDtoList.add(assesmentItemOptionDto);
+                			 }
+                			 if(j==12) {
+                				 assesmentItemDto.setMaxTimeAllowed(String.valueOf(cell.getNumericCellValue()));
+                			 }
+                			 if(j==13) {
+                				 assesmentItemDto.setAssessmentItemMarks((int)cell.getNumericCellValue());
+                			 }
+                			 if(j==14) {
+                				 assesmentItemDto.setAssessmentItemNegativeMarks((int)cell.getNumericCellValue());
+                			 }
+                    	}
+                     }
+				}
+				//assesmentItemOptionDtoList.get(assesmentItemDto.getCorrectOption()-1).setCorrectAnswer("YES");
+				if(assesmentItemOptionDtoList!=null && assesmentItemOptionDtoList.size()>0) {
+					assesmentItemOptionDtoList.get(assesmentItemDto.getCorrectOption()-1).setCorrectAnswer("YES");
+				    assesmentItemDtoMap.put(assesmentItemDto, assesmentItemOptionDtoList);
+				}
+			}
+			rowIndex++;
+		}
+		ObjectMapper mapper = new ObjectMapper();
+		System.out.println(mapper.writeValueAsString(assesmentItemDtoMap));
+		return assesmentItemDtoMap;
 	}
 	
 	
